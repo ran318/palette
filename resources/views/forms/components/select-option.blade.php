4ddfbf2b87542cd12d7aa668e679c70f1853969e
Adam Weston <awcodes1@gmail.com>
2025-03-07 13:19:16 -0500

    Chore: better select styling

--- resources/views/forms/components/select-option.blade.php
+++ resources/views/forms/components/select-option.blade.php
@@ -1,8 +1,32 @@
-<span style="display: flex; gap: 0.5rem; align-items: center; line-height: 1;">
+<div class="palette-select-option">
     @if ($color['type'] === 'class')
-        <span class="{{ $color['value'] }}" style="display: inline-block; border-radius: 100%; width: 0.875rem; height: 0.875rem; flex-shrink: 0;"></span>
+        <span class="{{ $color['value'] }}" style="display: block; border-radius: 100%; width: 1rem; height: 1rem; flex-shrink: 0;"></span>
     @else
-        <span style="display: inline-block; border-radius: 100%; width: 0.875rem; height: 0.875rem; flex-shrink: 0; background-color: {{ $color['type'] === 'rgb' ? 'rgba(' . $color['value'] . ', 1)' : $color['value'] }};"></span>
+        <span style="display: block; border-radius: 100%; width: 1rem; height: 1rem; flex-shrink: 0; background-color: {{ $color['type'] === 'rgb' ? 'rgba(' . $color['value'] . ', 1)' : $color['value'] }};"></span>
     @endif
-    <span>{{ $color['label'] }}</span>
-</span>
+    <span style="line-height: 1; display: block; height: 1rem;">{{ $color['label'] }}</span>
+</div>
+
+@once
+@push('styles')
+    <style>
+        .choices__inner .palette-select-option {
+            display: flex;
+            gap: 0.5rem;
+            align-items: center;
+            line-height: 1;
+            height: 1rem;
+            position: absolute;
+            top: calc(50% - 0.5rem);
+        }
+
+        .choices__list--dropdown .palette-select-option {
+            display: flex;
+            gap: 0.5rem;
+            align-items: center;
+            line-height: 1;
+            height: 1rem;
+        }
+    </style>
+@endpush
+@endonce
